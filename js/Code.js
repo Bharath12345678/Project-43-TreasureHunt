9f99e60a2ca3190fc5f5357cfa0399aa93281f9a
Bharath12345678 <73125420+Bharath12345678@users.noreply.github.com>
2021-05-21 13:46:01 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "PLATYPUS";
+const accessCode2 = "OCTOPUS";
+const accessCode3 = "TRIANGLE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("P P L T Y A S U", 100,50)      
+    fill("lightblue")
+    text("Hint: A mammal who lay eggs", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O O P C S T U", 700,150)
+    fill("lightblue")
+    text("Hint: Which animals blood is blue in colour", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("R A G E L N T I", 100,250)      
+    fill("lightblue")
+    text("Hint: Least sided polygon", 100,270)
+
+}
